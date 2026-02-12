95576923420ceeaf7969fe2aec73cac30f502031
The Vex Node <238744647+VexNode@users.noreply.github.com>
2026-02-12 16:15:07 +0300

    Create pathfinder.php

---
+++ pathfinder.php
@@ -0,0 +1,7 @@
+<?php
+// VexNode PathFinder - Server Info Tool
+echo "VexPathFinder PHP Analyzer\n";
+echo "Server OS: " . PHP_OS . "\n";
+echo "PHP Version: " . PHP_VERSION . "\n";
+echo "User: " . get_current_user() . "\n";
+?>
